f0ceca5c64ba73c428b7aa9f86d47694e34cdd46
Daniela <danielabedoya1674@gmail.com>
2018-10-26 12:00:18 -0500

    Probando editar base de datos, still not working

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express'),
   consolidate = require('consolidate'),
   ObjectID = require('mongodb').ObjectID,
-  MongoClient = require('mongodb').MongoClient;
+  MongoClient = require('mongodb').MongoClient,
+  bodyParser = require('body-parser');
 //npm install handlebars
 var app = express();
 
@@ -24,6 +25,7 @@ MongoClient.connect('mongodb://localhost:27017', (err, client) => {
 //Nombre de la coleccion
 //---------------------------------
 var dbName = 'products';
+var dbVenta = 'ventas';
 
 app.get('/', function (req, res) {
 
@@ -55,20 +57,48 @@ app.get('/checkout', (req, res) => {
 });
 
 app.get('/buyedProducts', (req, res) => {
-  console.log(req.query.ids);
   var arreglo = req.query.ids.split(',');
   arreglo = arreglo.map(function (id) {
-      return new ObjectID(id);
+    return new ObjectID(id);
   });
   db.collection('products')
-      .find({
-          _id: {
-              $in: arreglo 
-          }
-      })
-      .toArray((err, result) => {
-          res.send(result);
-      });
+    .find({
+      _id: {
+        $in: arreglo
+      }
+    })
+    .toArray((err, result) => {
+      res.send(result);
+    });
+});
+
+app.post('/addData', (req, res) => {
+  console.log("Triggered");
+  res.redirect('/test/hola');
+  /* if (req.body.name)
+     var name = req.body.name;
+   if (req.body.cedula)
+     var cedula = req.body.cedula;
+   if (req.body.direccion)
+     var direccion = req.body.direccion;
+ 
+   console.log(pago + ":" + name + ":" + cedula + ":" + direccion);
+ 
+   res.redirect('/test/' + id);
+   try {
+     db.collection(dbVenta).insertOne({
+       nombre: name,
+       direccion: direccion,
+       cc: cedula,
+       pago: pago
+     });
+   } catch (e) {
+     print(e);
+   }*/
+});
+
+app.get('/test/:id', (req, res) => {
+  res.send("Muchas gracias por la compra");
 });
 
 app.listen(3000, function () {
@@ -77,5 +107,5 @@ app.listen(3000, function () {
 
 //npm init
 //npm install express handlebars consolidate 
-// mongo/mongod/mongodb
+//mongo/mongod/mongodb
 //Node index
