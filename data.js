fa53837028587fa0029f9c7e7b43fc00ff56e2ac
Daniela <danielabedoya1674@gmail.com>
2018-10-26 23:29:10 -0500

    Cambiando los filtros

--- data.js
+++ data.js
@@ -5,7 +5,8 @@
     "price" : 20,
     "year" : 2008,
     "data" : "Built with high quality materials and designed to make the most of the amazing Wacom Pro Pen 2, Wacom Intuos Pro is our finest creative pen tablet to date. With built-in Bluetooth connectivity and a sleek new design, it’s easy to set-up and looks great on your desk. Start creating your next stand-out project",
-    "name" : "Intuos PRO"
+    "name" : "Intuos PRO",
+    "free" : false
 }
 
 /* 2 */
@@ -14,7 +15,8 @@
     "price" : 50,
     "year" : 2009,
     "data" : "Explore new ways to get creative with Wacom Intuos With a light, super-accurate pen and free downloadable software* to suit your style, Wacom Intuos is built to bring your wildest ideas to life.",
-    "name" : "Intuos"
+    "name" : "Intuos",
+    "free" : false
 }
 
 /* 3 */
@@ -23,7 +25,8 @@
     "price" : 23,
     "year" : 2010,
     "data" : "Designed to give you the freedom to create anywhere Wacom MobileStudio Pro packs a whole studio of creative tools into a lightweight mobile pen computer featuring the new Wacom Pro Pen 2. Move seamlessly through your workflow from 2D to 3D wherever you want to create.",
-    "name" : "Mobile Studio Pro"
+    "name" : "Mobile Studio Pro",
+    "free" : true
 }
 
 /* 4 */
@@ -32,7 +35,8 @@
     "price" : 12,
     "year" : 2011,
     "data" : "Gain all the innovations of our professional pen tablets, now with the ability to work directly on the screen of this compact Cintiq 13HD. Great color and resolution, professional ergonomics, and pressure-sensitive control are all together in an affordable design.",
-    "name" : "Cintiq 13HD"
+    "name" : "Cintiq 13HD",
+    "free" : false
 }
 
 /* 5 */
@@ -41,7 +45,8 @@
     "price" : 43,
     "year" : 2012,
     "data" : "Add multi-touch gesture support to one of Wacom's most versatile creative pen displays. You are sure to enjoy creating directly on this large-format, high-definition display with Wacom's pressure-sensitive pen. Now you can also dive both hands into your work to rotate, zoom in, and work more fluidly. Ample screen size, advanced display performance, and multi-touch support, all in one display.",
-    "name" : "Cintiq 22HD touch"
+    "name" : "Cintiq 22HD touch",
+    "free" : true
 }
 
 /* 6 */
@@ -50,7 +55,8 @@
     "price" : 70,
     "year" : 2013,
     "data" : "Tools might transform, but artistry remains. The Cintiq 27QHD creative pen & touch display sets a new standard in color and resolution. The natural forms of creative expression meet an advanced interface design that re-imagines the creative workflow so you don't have to adapt to the tool—the tool adapts to you. Work with hands-on fluidity as you create with a pressure-sensitive pen and multi-touch gestures directly on an expansive edge-less glass display.",
-    "name" : "Cintiq 27QHD touch"
+    "name" : "Cintiq 27QHD touch",
+    "free" : true
 }
 
 /* 7 */
@@ -59,7 +65,8 @@
     "price" : 99,
     "year" : 2014,
     "data" : "The Cintiq Pro family is getting bigger. Joining the Cintiq Pro 16 and 13, the 32 and 24 are advanced creative pen displays, featuring premium 4K* screens with world class color performance and virtually no parallax. Combined with Wacom Pro Pen 2, they offer an experience designed to enhance every creative breakthrough.",
-    "name" : "Cintiq Pro"
+    "name" : "Cintiq Pro",
+    "free" : true
 }
 
 /* 8 */
@@ -68,7 +75,8 @@
     "price" : 87,
     "year" : 2015,
     "data" : "Take handwritten notes and ideas further with Bamboo Slate. Write naturally with pen on any paper, then store, shape and share your notes and ideas in the cloud.",
-    "name" : "Bamboo Slate"
+    "name" : "Bamboo Slate",
+    "free" : false
 }
 
 /* 9 */
@@ -77,7 +85,8 @@
     "price" : 34,
     "year" : 2016,
     "data" : "Work smarter with Bamboo Folio. Write naturally with pen on any paper, then store, shape and share your notes and ideas in the cloud.",
-    "name" : "Bamboo Folio"
+    "name" : "Bamboo Folio",
+    "free" : true
 }
 
 /* 10 */
@@ -86,5 +95,6 @@
     "price" : 76,
     "year" : 2017,
     "data" : "Just power up and start writing. Bamboo Tip is a fine-tip stylus for quickly noting ideas on your iPhone, iPad and Android devices.",
-    "name" : "Stylus"
+    "name" : "Stylus",
+    "free" : false
 }
